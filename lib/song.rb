6fbfd55e7d5a86ce14a926ad9b5c6e7864d65c70
Dominique Skaf <doskaf@yahoo.com>
2019-09-19 21:17:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,19 @@
 class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
   def initialize(name, artist, genre)
-end
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres = []
+    @@genres << genre
+  end
+  def count
+    @@count
+  end
+  def genres
+    @@genres.each do |genre|
+      if 
+  end
 end
