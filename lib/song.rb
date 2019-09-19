95a8cf2c29ca27111a6053610c14cef1cc5e5d79
Dominique Skaf <doskaf@yahoo.com>
2019-09-19 21:26:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres = []
     @@genres << genre
-    @@artists = []
     @@artists << artist
   end
   
